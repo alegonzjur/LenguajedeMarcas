b1bf1afa5b1431ddb1227e7393f7a9308eefbbc3
alegonzjur <alegonjur@gmail.com>
2023-05-22 19:25:13 +0200

    Ej 10 PL

--- SQL/PL/Ej_clase.sql
+++ SQL/PL/Ej_clase.sql
@@ -52,3 +52,79 @@ BEGIN
     end if;
 END PROCEDURE;
 
+/*Ejercicio 4*/
+DECLARE CURSOR c_libros IS 
+    SELECT titulo FROM Libros;
+    i NUMBER := 0;
+begin
+  FOR libro IN c_libros loop
+    i := i+1;
+    DBMS_OUTPUT.PUT_LINE(libro.titulo);
+    EXIT WHEN i = 10;
+  end loop;
+end;
+
+/*Ejercicio 5*/
+DECLARE CURSOR C_libros IS 
+    SELECT titulo FROM LIBROS;
+    i number := 0;
+BEGIN 
+    loop
+      
+      WHILE i<10 
+      DBMS_OUTPUT.PUT_LINE(libro.titulo);
+      i := i+1;  
+    end loop
+end;
+
+/*Ejercicio 6*/
+DECLARE
+    p_id_cliente NUMBER;
+    CURSOR C_pedidos (p_id_cliente) IS 
+        select titulo
+          from Pedidos join Libros ON Pedidos.id_libro = Libros.id_libro
+         where id_cliente = p.id_cliente;
+BEGIN 
+    FOR registro IN C_pedidos loop
+    DBMS_OUTPUT.PUT_LINE(id_cliente.titulo)
+    end loop
+END;
+
+/*Ejercicio 7*/
+DECLARE
+    p_libro NUMBER;
+    p_id_libro NUMBER := 'L001';
+BEGIN 
+    SELECT precio INTO p_libro FROM Libros WHERE id_libro = p_id_libro;
+    DBMS_OUTPUT.put_line(p_libro);
+    exception
+      when no_data_found then
+        DBMS_OUTPUT.PUT_LINE('El libro no existe');
+END;
+
+/*Ejercicio 8*/
+CREATE OR REPLACE 
+PROCEDURE crea_correo(correo_cliente VARCHAR2) IS
+c_correo VARCHAR2;
+    
+BEGIN
+    select correo
+      into c_correo
+      from Clientes
+     where correo = correo_cliente;
+    if c_correo%no_data_found EXISTS then
+      insert into Clientes (correo)
+      values ('vinicius.junior.@iesciudadjardin.com')
+    else
+      DBMS_OUTPUT.DBMS_OUTPUT('El correo' || c_correo || 'ya existe.')
+    end if;
+END crea_correo;
+
+/*Ejercicio 9*/
+CREATE OR REPLACE 
+function libros_total RETURN num_libros NUMBER;
+BEGIN 
+    SELECT COUNT(DISTINCT id_libro) INTO num_libros FROM Libros;
+    RETURN num_libros;
+END ;
+/*Ejercicio 10*/
